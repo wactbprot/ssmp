71f8992b6f643e5e5bfe63b59c186c18220b742d
wactbprot <thsteinbock@web.de>
2014-11-16 22:41:32 +0100

    eowe

--- mem.js
+++ mem.js
@@ -1,9 +1,14 @@
-var ndata   = require("ndata"),
-    prog    = require("commander")
+var ndata   = require("ndata")
+  , prog    = require("commander")
+  , _       = require("underscore")
+  , bunyan  = require("bunyan")
+  , deflt   = require("./lib/default")
+  , log     = bunyan.createLogger({name: deflt.appname})
+  , ok      = {ok:true};
 
 prog.version("0.1")
 .option("-P, --port <port>", "port (default is  9000)", parseInt)
 .parse(process.argv);
 
-var  port  = prog.ndataport || 9000
-  , dataServer = ndata.createServer({port: port})
+var port  = prog.ndataport || 9000
+  , ds   = ndata.createServer({port: port});
