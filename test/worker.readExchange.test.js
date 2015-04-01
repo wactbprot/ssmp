a16f8ca37e1b3aef589830ab637ad2958c6741f1
wactbprot <thsteinbock@web.de>
2015-04-01 15:51:31 +0200

    ..

---
+++ test/worker.readExchange.test.js
@@ -0,0 +1,93 @@
+var _        = require("underscore")
+  , assert   = require("assert")
+  , ndata    = require("ndata")
+  , deflt    = require("../lib/default")
+  , worker
+  , ds
+  , mem
+  , exchpath   = ["test","exchange"];
+
+describe('worker', function(){
+  before(function(done){
+    ds = ndata.createServer({port: deflt.mem.port}).on('ready', function(){
+           mem  = ndata.createClient({port: deflt.mem.port});
+           mem.set(exchpath.concat(["a"])
+                  , {Value:
+                     {value:"ok"},
+                     Ready:
+                     {value:"true"}}
+                  , function(){
+                      mem.set(exchpath.concat(["b"])
+                             , {Value:
+                                {value:1,
+                                 type:"number",
+                                 save:true},
+                                Unit:
+                                {value:"mbar",
+                                 save:true},
+                                Ready:
+                                {value:true}}
+                             , function(){
+                                 mem.set(exchpath.concat(["c"])
+                                        , {Wrong:
+                                           {value:"nope"}}
+                                        , function(){
+                                            worker    = require("../lib/worker")
+                                            done();
+                                          });
+                               });
+                    });
+         });
+  });
+
+  after(function(done){
+    ds.destroy();
+    done();
+  });
+
+  describe('#readExchange(task, cb)', function(){
+
+    it('should fail on missing key', function(done){
+      worker.readExchange({}, function(res){
+
+        assert.equal(res.error, "not a valid task")
+        done();
+      });
+    });
+
+    it('should fail on wrong key', function(done){
+
+      worker.readExchange({Key:"wrong", Path:["test", 0]}, function(res){
+        assert.equal(res.error, "nothing below give key")
+        done();
+      });
+    });
+
+    it('should work without .save entries', function(done){
+
+      worker.readExchange({Key:"a", Path:["test", 0]}, function(res){
+        assert.equal(res.ok, true);
+        done();
+      });
+    });
+
+    it('should work with type:number', function(done){
+
+      worker.readExchange({Key:"b", Path:["test", 0]}, function(res){
+        assert.equal(res.ok, true);
+        done();
+      });
+    });
+
+    it('should  work on missing Ready ', function(done){
+
+      worker.readExchange({Key:"c", Path:["test", 0]}, function(res){
+        assert.equal(res.ok, true);
+        done();
+      });
+    });
+
+
+});
+
+});
