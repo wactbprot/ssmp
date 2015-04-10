c4d8a62e625e52c3d0192c0de2b4b2d1ca929ee6
wactbprot <thsteinbock@web.de>
2015-04-10 22:54:02 +0200

    on the way to heal observe()

--- test/observe.test.js
+++ test/observe.test.js
@@ -35,6 +35,77 @@ describe('observe', function(){
     });
   });
 
+  describe('#dispatch()', function(){
+    it('should return error', function(done){
+      observe.dispatch(["test", 0], "", "", function(err, path){
+        console.log(err)
+        console.log(path)
+
+        //        assert.equal(res.ok, true);
+        done();
+      });
+    });
+
+    it('should return error', function(done){
+      observe.dispatch([], "", "", function(err, path){
+        assert.equal(err, "wrong path");
+        done();
+      });
+    });
+
+  });
+
+
+  describe('#time_to_exchange()', function(){
+    it('should return error', function(done){
+      observe.time_to_exchange([], false, function(err, path){
+        assert.equal(err, "wrong path");
+        done();
+      });
+    });
+
+    it('should work with true', function(done){
+      observe.time_to_exchange(["test"], true, function(err, path){
+        assert.equal(err, false);
+        done();
+      });
+    });
+
+    it('should work with false', function(done){
+      observe.time_to_exchange(["test"], false, function(err, path){
+        assert.equal(err, false);
+        done();
+      });
+    });
+
+  });
+
+  describe('#shout()', function(){
+    it('should work with no newstr', function(done){
+      observe.shout(["test", 0],"","","",function(err, path){
+        assert.equal(err, false);
+        assert.equal(path[0],"test");
+        done();
+      });
+    });
+
+    it('should work with  newstr', function(done){
+      observe.shout(["test", 0],"","", "new", function(err, path){
+        assert.equal(err, false);
+        assert.equal(path[0],"test");
+        done();
+      });
+    });
+
+    it('should return error', function(done){
+      observe.shout([],"","","",function(err, path){
+        assert.equal(err, "wrong path");
+        done();
+      });
+    });
+
+  });
+
   describe('#stop_cont()', function(){
     it('should return error (path)', function(done){
       observe.stop_cont([], "", {}, function(err, path){
@@ -57,6 +128,60 @@ describe('observe', function(){
       });
     });
 
+    it('should work for one container', function(done){
+      observe.stop_cont(["test", 0], "stop_container_obs", {test:{0:"test"}}, function(err, path){
+        assert.equal(err, false);
+        assert.equal(path[0], "test");
+        done();
+      });
+    });
+
+    it('should work for all containers', function(done){
+      observe.stop_cont(["test", 0], "stop_all_container_obs", {test:{0:"test"}}, function(err, path){
+        assert.equal(err, false);
+        assert.equal(path[0], "test");
+        done();
+      });
+    });
+
   });
+
+  describe('#cmd_to_array()', function(){
+    it('should return an array containing the given string', function(){
+      var  r   = "run";
+      assert.equal(true, _.isArray(observe.cmd_to_array(r)));
+      assert.equal(r, observe.cmd_to_array(r)[0]);
+      assert.equal(1, observe.cmd_to_array(r).length);
+    })
+    it('should return an array containing the given string', function(){
+      var  r   = "mon";
+      assert.equal(true, _.isArray(observe.cmd_to_array(r)));
+      assert.equal(r, observe.cmd_to_array(r)[0]);
+      assert.equal(1, observe.cmd_to_array(r).length);
+    })
+    it('should return an array of length 2', function(){
+      var l   = "load",
+          r   = "run",
+          lr  = "load;run";
+      assert.equal(true, _.isArray(observe.cmd_to_array(lr)));
+      assert.equal(l, observe.cmd_to_array(lr)[0]);
+      assert.equal(r, observe.cmd_to_array(lr)[1]);
+      assert.equal(2, observe.cmd_to_array(lr).length);
+    })
+    it('should return an array of length 3', function(){
+      var l   = "load",
+          r   = "run",
+          lrr  = "load;2:run";
+      assert.equal(true, _.isArray(observe.cmd_to_array(lrr)));
+      assert.equal(l, observe.cmd_to_array(lrr)[0]);
+      assert.equal(r, observe.cmd_to_array(lrr)[1]);
+      assert.equal(r, observe.cmd_to_array(lrr)[2]);
+      assert.equal(3, observe.cmd_to_array(lrr).length);
+    })
+    it('should return an empty string at position 0', function(){
+      assert.equal(true, _.isArray(observe.cmd_to_array("")));
+      assert.equal("", observe.cmd_to_array("")[0]);
+    })
+  })
 
 });
--- test/utils.test.js
+++ test/utils.test.js
@@ -13,43 +13,6 @@ req.params.id = id;
 req.body      = "load";
 
 describe('utils', function(){
-  describe('#cmd_to_array(cmdstr)', function(){
-    it('should return an array containing the given string', function(){
-      var  r   = "run";
-      assert.equal(true, _.isArray(utils.cmd_to_array(r)));
-      assert.equal(r, utils.cmd_to_array(r)[0]);
-      assert.equal(1, utils.cmd_to_array(r).length);
-    })
-    it('should return an array containing the given string', function(){
-      var  r   = "mon";
-      assert.equal(true, _.isArray(utils.cmd_to_array(r)));
-      assert.equal(r, utils.cmd_to_array(r)[0]);
-      assert.equal(1, utils.cmd_to_array(r).length);
-    })
-    it('should return an array of length 2', function(){
-      var l   = "load",
-          r   = "run",
-          lr  = "load;run";
-      assert.equal(true, _.isArray(utils.cmd_to_array(lr)));
-      assert.equal(l, utils.cmd_to_array(lr)[0]);
-      assert.equal(r, utils.cmd_to_array(lr)[1]);
-      assert.equal(2, utils.cmd_to_array(lr).length);
-    })
-    it('should return an array of length 3', function(){
-      var l   = "load",
-          r   = "run",
-          lrr  = "load;2:run";
-      assert.equal(true, _.isArray(utils.cmd_to_array(lrr)));
-      assert.equal(l, utils.cmd_to_array(lrr)[0]);
-      assert.equal(r, utils.cmd_to_array(lrr)[1]);
-      assert.equal(r, utils.cmd_to_array(lrr)[2]);
-      assert.equal(3, utils.cmd_to_array(lrr).length);
-    })
-    it('should return an empty string at position 0', function(){
-      assert.equal(true, _.isArray(utils.cmd_to_array("")));
-      assert.equal("", utils.cmd_to_array("")[0]);
-    })
-  })
 
   describe('#vl_date()', function(){
     it('should return the wellknown date format', function(){
