0d2101d4a141f3dac7e0eb98c4ac83396dac97a8
wactbprot <thsteinbock@web.de>
2015-04-17 14:49:48 +0200

    refactor insert()

--- test/load.insert.test.js
+++ test/load.insert.test.js
@@ -23,35 +23,57 @@ describe('load', function(){
 
   describe('#insert(def, calob, cb)', function(){
 
+    it('should return erron on wrong definition (str)', function(done){
+      load.insert("def", {}, function(err, res){
+        assert.equal(err,"wrong definition");
+        done();
+      });
+    });
+
+
+    it('should return erron on wrong definition ([str])', function(done){
+      load.insert(["def"], {}, function(err, res){
+        assert.equal(err,"wrong definition");
+        done();
+      });
+    });
+
+    it('should return erron on wrong definition ([[str]])', function(done){
+      load.insert(["def"], {}, function(err, res){
+        assert.equal(err,"wrong definition");
+        done();
+      });
+    });
+
     it('should add Id array', function(done){
       var def = [[{"TaskName":"A"},{"TaskName":"B"},{"TaskName":"C"}]]
-      load.insert(def, {}, function(res){
+      load.insert(def, {}, function(err, res){
         assert.equal(_.isArray(res[0][0].Id), true)
         assert.equal(_.isArray(res[0][1].Id), true)
         assert.equal(_.isArray(res[0][2].Id), true)
-        done()
-      })
+        done();
+      });
     });
 
 
      it('should work with 3 parallel tasks at seq/position 0', function(done){
       var def = [[{"TaskName":"A"},{"TaskName":"B"},{"TaskName":"C"}]]
-      load.insert(def, {}, function(res){
-       assert.equal(res[0][0].TaskName, "A")
-       assert.equal(res[0][1].TaskName, "B")
-       assert.equal(res[0][2].TaskName, "C")
-        done()
-      })
-    });
+       load.insert(def, {}, function(err, res){
+         assert.equal(res[0][0].TaskName, "A")
+         assert.equal(res[0][1].TaskName, "B")
+         assert.equal(res[0][2].TaskName, "C")
+         done();
+       });
+     });
 
     it('should work with 3 parallel tasks at seq/position 1', function(done){
       var def = [[{"TaskName":"A"}],
                  [{"TaskName":"A"},{"TaskName":"B"},{"TaskName":"C"}]]
-      load.insert(def, {}, function(res){
-       assert.equal(res[1][0].TaskName, "A")
-       assert.equal(res[1][1].TaskName, "B")
-       assert.equal(res[1][2].TaskName, "C")
-        done()
+      load.insert(def, {}, function(err, res){
+        assert.equal(res[1][0].TaskName, "A")
+        assert.equal(res[1][1].TaskName, "B")
+        assert.equal(res[1][2].TaskName, "C")
+        done();
       });
     });
 
@@ -59,11 +81,11 @@ describe('load', function(){
       var def = [[{"TaskName":"A"}],
                  [{"TaskName":"A"}],
                  [{"TaskName":"A"},{"TaskName":"B"},{"TaskName":"C"},{"TaskName":"D"}]]
-      load.insert(def, {}, function(res){
+      load.insert(def, {}, function(err, res){
         assert.equal(res[2][0].TaskName, "A")
         assert.equal(res[2][1].TaskName, "B")
         assert.equal(res[2][2].TaskName, "C")
-        done()
+        done();
       });
     });
   });
