9422eef3254a2202c0fe4bd9f2f43b3295813175
wactbprot <thsteinbock@web.de>
2015-05-20 13:24:50 +0200

    - get rid of static + new command line param --load mpid 	modified:   package.json 	modified:   ssmp.js 	deleted:    static/check.json

--- ssmp.js
+++ ssmp.js
@@ -9,9 +9,8 @@ var ssmp = function(){
     , log     = bunyan.createLogger({name: deflt.appname})
     , ok      = {ok:true};
 
-  prog.version("0.3")
-  .option("-a, --all_statics", "load all statics on start up.")
-  .option("-l, --statics_list <slist>", "name1,name2 (see folder ssmp/statics for available names)")
+  prog.version("0.4")
+  .option("-l, --load <mpid>", "the id of an mpd")
   .parse(process.argv);
 
 
@@ -35,56 +34,23 @@ var ssmp = function(){
 
       require("./http-api/server")(deflt, function(){
         var mem      = ndata.createClient({port: deflt.mem.port})
-          , statics  = {};
-        if(prog.all_statics || prog.statics_list){
-          var sts  = utils.get_jsn("./static/");
-          if(prog.statics_list){
-            var sl =  prog.statics_list.split(",")
-            for(var j = 0; j < sl.length; j++){
-              var sname = sl[j]
-              if(sts[sname]){
-                statics[sname] = sts[sname];
-              }
-            }
-          }else{
-            statics = sts;
-          }
-        }
-
         load.ini(function(){
           run.ini(function(){
             build.ini(function(){
               observe.ini(function(){
                 mphandle.ini(function(){
                   cdhandle.ini(function(){
-                    for(var i in statics){
-                      var mpi = statics[i];
-                      mem.set([i] ,mpi , function(err){
+                    if(prog.load){
+                      mem.publish("get_mp", prog.load , function(err){
                         if(!err){
-                          if(mpi.meta && mpi.meta.container &&  mpi.meta.container.N){
-                            var cN = mpi.meta.container.N;
-                            for(var j = 0; j < cN; j++){
-                              (function(k, l){
-                                mem.publish("start_container_obs", [k, l], function(err){
-                                  if(!err){
-                                    log.info(ok
-                                            , "start_container_obs event published for mp: "
-                                            + k +" container: " + l + ", exec callback");
-                                  }else{
-                                    log.error({error:err}
-                                             , "error on publishing build event")
-                                  }
-                                });
-                              }(i,j))
-                            }
-                          }
+                          log.info(ok
+                                  , " published to get_mp channel");
                         }else{
-                          log.info({error:err}
-                                  , "unable to set static " + i);
+                          log.error({error:err}
+                                  , "failed to published to get_mp channel");
                         }
-                      }); // publish
-                    } // for
-
+                      });
+                    }
                   });
                 });
               });
